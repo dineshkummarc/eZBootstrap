0f74545adf696d6b853cdcb858310786894ccfa5
Damien MARTIN <damien@martin-damien.fr>
2012-03-15 16:04:25 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# eZBootstrap
+
+> **WARNING : This extension is a work in progress.**
+> 
+> See [http://twitter.github.com/bootstrap](http://twitter.github.com/bootstrap) for further informations.
+
+Add `bootstrap` in your eZ Publish website.
+
+## TODO
+
+* Implement all classes and blocks types
+* Tables must add a `theader` around the `th` tags.
+* A lot of things…
---
+++ settings/content.ini.append.php
@@ -0,0 +1,15 @@
+<?php /* #?ini charset="utf-8"?
+
+[table]
+AvailableClasses[]=table
+AvailableClasses[]=table-bordered
+AvailableClasses[]=table-striped
+AvailableClasses[]=table-condensed
+AvailableClasses[]=table table-bordered table-striped
+ClassDescription[table]=Basic
+ClassDescription[table-bordered]=Bordered
+ClassDescription[table-striped]=Zebra-stripe
+ClassDescription[table-condensed]=Condensed
+ClassDescription[table table-bordered table-striped]=Basic + Bordered + Zebra-striped
+
+*/ ?>
